fe238dce89427bb8152da4fe5270e744efe7354c
Denis West <DenisWest@outlook.com>
2022-11-16 05:03:58 -0600

    feat: Estableciendo coordenadas de Nicaragua

--- Hackathon2022/Views/GoogleMapPage.xaml.cs
+++ Hackathon2022/Views/GoogleMapPage.xaml.cs
@@ -2,8 +2,11 @@ namespace Hackathon2022.Views;
 
 public partial class GoogleMapPage : ContentPage
 {
-	public GoogleMapPage()
-	{
-		InitializeComponent();
-	}
+    public GoogleMapPage()
+    {
+        InitializeComponent();
+
+        Mappy.MoveToRegion(new Microsoft.Maui.Maps.MapSpan(
+            new Location(12.865416, -85.207229), 12.8654, 85.2072));
+    }
 }
