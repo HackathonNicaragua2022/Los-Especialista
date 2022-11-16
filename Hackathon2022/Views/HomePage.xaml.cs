5c0ca7046bdf1f8c94959bd383b5b5ccb366310c
Denis West <DenisWest@outlook.com>
2022-11-16 03:28:02 -0600

    feat: Página se inicio de sesión de usuario existente

---
+++ Hackathon2022/Views/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Hackathon2022.Views;
+
+public partial class HomePage : ContentPage
+{
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+}
--- Hackathon2022/Views/LoginPage.xaml.cs
+++ Hackathon2022/Views/LoginPage.xaml.cs
@@ -2,8 +2,23 @@ namespace Hackathon2022.Views;
 
 public partial class LoginPage : ContentPage
 {
-	public LoginPage()
-	{
-		InitializeComponent();
-	}
+    public LoginPage()
+    {
+        InitializeComponent();
+        ModifyEntry();
+    }
+
+    void ModifyEntry()
+    {
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("MyCustomization", (Handler, View) =>
+        {
+            const int PaddingTopBottom = 5;
+            const int PaddingLeftRight = 15;
+
+            if (View is Entry)
+            {
+                Handler.PlatformView.SetPadding(PaddingLeftRight, PaddingTopBottom, PaddingLeftRight, PaddingTopBottom);
+            }
+        });
+    }
 }
