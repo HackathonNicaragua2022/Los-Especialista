5d324c3c4525c19f7915c7bac3dff77906696c99
Denis West <DenisWest@outlook.com>
2022-11-25 16:21:36 -0600

    feat: Routes and Coordinates Controllers

---
+++ HackathonBackend/Controllers/CoordinatesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HackathonBackend;
+using HackathonBackend.Models;
+
+namespace HackathonBackend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CoordinatesController : ControllerBase
+{
+    private readonly DestinyContext Database;
+
+    public CoordinatesController(DestinyContext Database)
+    {
+        this.Database = Database;
+    }
+
+    // GET: api/Coordinates
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Coordinate>>> GetCoordinates()
+    {
+        if (Database.Coordinates == null)
+        {
+            return NotFound();
+        }
+
+        return await Database.Coordinates.ToListAsync();
+    }
+
+    // GET: api/Coordinates/5
+    [HttpGet("{Id}")]
+    public async Task<ActionResult<Coordinate>> GetCoordinate(int Id)
+    {
+        if (Database.Coordinates == null)
+        {
+            return NotFound();
+        }
+
+        var Coordinate = await Database.Coordinates.FindAsync(Id);
+
+        if (Coordinate == null)
+        {
+            return NotFound();
+        }
+
+        return Coordinate;
+    }
+
+    // PUT: api/Coordinates/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{Id}")]
+    public async Task<IActionResult> PutCoordinate(int Id, Coordinate Coordinate)
+    {
+        if (Id != Coordinate.Id)
+        {
+            return BadRequest();
+        }
+
+        Database.Entry(Coordinate).State = EntityState.Modified;
+
+        try
+        {
+            await Database.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!CoordinateExists(Id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    // POST: api/Coordinates
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<Coordinate>> PostCoordinate(Coordinate Coordinate)
+    {
+        if (Database.Coordinates == null)
+        {
+            return Problem("Entity set 'DestinyContext.Coordinates'  is null.");
+        }
+
+        Database.Coordinates.Add(Coordinate);
+        await Database.SaveChangesAsync();
+
+        return CreatedAtAction("GetCoordinate", new { Id = Coordinate.Id }, Coordinate);
+    }
+
+    // DELETE: api/Coordinates/5
+    [HttpDelete("{Id}")]
+    public async Task<IActionResult> DeleteCoordinate(int Id)
+    {
+        if (Database.Coordinates == null)
+        {
+            return NotFound();
+        }
+
+        var Coordinate = await Database.Coordinates.FindAsync(Id);
+
+        if (Coordinate == null)
+        {
+            return NotFound();
+        }
+
+        Database.Coordinates.Remove(Coordinate);
+        await Database.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool CoordinateExists(int Id)
+    {
+        return (Database.Coordinates?.Any(C => C.Id == Id)).GetValueOrDefault();
+    }
+}
---
+++ HackathonBackend/Controllers/RoutesController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HackathonBackend;
+using HackathonBackend.Models;
+
+namespace HackathonBackend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RoutesController : ControllerBase
+{
+    private readonly DestinyContext Database;
+
+    public RoutesController(DestinyContext Database)
+    {
+        this.Database = Database;
+    }
+
+    // GET: api/Routes
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Routes>>> GetRoutes()
+    {
+        if (Database.Routes == null)
+        {
+            return NotFound();
+        }
+
+        return await Database.Routes.ToListAsync();
+    }
+
+    // GET: api/Routes/5
+    [HttpGet("{Id}")]
+    public async Task<ActionResult<Routes>> GetRoutes(string Id)
+    {
+        if (Database.Routes == null)
+        {
+            return NotFound();
+        }
+
+        var Routes = await Database.Routes.FindAsync(Id);
+
+        if (Routes == null)
+        {
+            return NotFound();
+        }
+
+        return Routes;
+    }
+
+    // PUT: api/Routes/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{Id}")]
+    public async Task<IActionResult> PutRoutes(string Id, Routes Routes)
+    {
+        if (Id != Routes.Id)
+        {
+            return BadRequest();
+        }
+
+        Database.Entry(Routes).State = EntityState.Modified;
+
+        try
+        {
+            await Database.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!RoutesExists(Id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    // POST: api/Routes
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<Routes>> PostRoutes(Routes Routes)
+    {
+        if (Database.Routes == null)
+        {
+            return Problem("Entity set 'DestinyContext.Routes'  is null.");
+        }
+
+        Database.Routes.Add(Routes);
+
+        try
+        {
+            await Database.SaveChangesAsync();
+        }
+        catch (DbUpdateException)
+        {
+            if (RoutesExists(Routes.Id))
+            {
+                return Conflict();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return CreatedAtAction("GetRoutes", new { Id = Routes.Id }, Routes);
+    }
+
+    // DELETE: api/Routes/5
+    [HttpDelete("{Id}")]
+    public async Task<IActionResult> DeleteRoutes(string Id)
+    {
+        if (Database.Routes == null)
+        {
+            return NotFound();
+        }
+
+        var Routes = await Database.Routes.FindAsync(Id);
+
+        if (Routes == null)
+        {
+            return NotFound();
+        }
+
+        Database.Routes.Remove(Routes);
+
+        await Database.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool RoutesExists(string Id)
+    {
+        return (Database.Routes?.Any(R => R.Id == Id)).GetValueOrDefault();
+    }
+}
