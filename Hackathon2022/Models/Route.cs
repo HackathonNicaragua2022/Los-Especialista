b3ad588a709b88444c9264d475b6c9782a5a8a31
Denis West <DenisWest@outlook.com>
2022-11-26 11:23:02 -0600

    feat: Favorite View

---
+++ Hackathon2022/Models/Route.cs
@@ -0,0 +1,50 @@
+﻿namespace Hackathon2022.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Route
+    {
+        public string Cooperativa { get; set; }
+
+        public string TipoTransporte { get; set; } = "Autobús";
+
+        public string NombreRuta { get; set; }
+
+        public string Referencia { get; set; }
+    }
+
+    public static class RoutesData
+    {
+        public static IList<Route> Routes { get; private set; }
+
+        private static async void LoadRoutes()
+        {
+            Routes = new List<Route>();
+
+            var Stream = await FileSystem.OpenAppPackageFileAsync("NicaraguaManagua.geojson");
+            var JsonDoc = System.Text.Json.JsonDocument.Parse(Stream);
+            var Features = JsonDoc.RootElement.GetProperty("features").EnumerateArray();
+
+            foreach (var Feature in Features)
+            {
+                var Properties = Feature.GetProperty("properties");
+
+                Routes.Add(new Route
+                {
+                    NombreRuta = Properties.GetProperty("name").GetString(),
+                    Cooperativa = Properties.GetProperty("operator").GetString(),
+                    Referencia = Properties.GetProperty("ref").GetString()
+                });
+            }
+        }
+
+        static RoutesData()
+        {
+            LoadRoutes();
+        }
+    }
+}
